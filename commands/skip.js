566d58d6e0dc49e661e2c3de64a936e2a7c82d02
SunshineRises <74737666+SunshineRises@users.noreply.github.com>
2021-01-25 11:30:23 +0100

    Create skip.js

---
+++ commands/skip.js
@@ -0,0 +1,21 @@
+const distube = require('distube')
+
+
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
+
+    let queue = await bot.distube.getQueue(message);
+
+    if(queue) {
+        bot.distube.skip(message)
+
+        message.reply('DONE!')
+    } else if (!queue) {
+        return
+    };
+}
+
+module.exports.config = {
+    name: "skip",
+    aliases: ["s"]
+}
