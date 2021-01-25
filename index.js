25b9dca966eb8b100894aa937de0e9c21ab6976d
SunshineRises <74737666+SunshineRises@users.noreply.github.com>
2021-01-25 11:21:40 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client({
+	partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+});
+const config = require('./settings.json');
+const { loadCommands } = require('./utils/commands');
+const DisTube = require('distube')
+const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+bot.distube = new DisTube(bot, { searchSongs: false, emitNewSongOnly: true });
+
+    bot.distube
+        .on("playSong", (message, queue, song) => message.channel.send(
+            `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}`
+        ))
+        .on("addSong", (message, queue, song) => message.channel.send(
+            `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+        ))
+        .on("playList", (message, queue, playlist, song) => message.channel.send(
+            `Play \`${playlist.name}\` playlist (${playlist.songs.length} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
+        ))
+require('./utils/events')(bot);
+
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+loadCommands(bot);
+
+bot.login(config.token);
